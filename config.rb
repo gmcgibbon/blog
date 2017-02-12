8c5f2471eadd6d875c87fe015cc929b8b5328c34
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-02-12 02:53:49 -0600

    Move default layout to layouts/application

--- config.rb
+++ config.rb
@@ -2,6 +2,8 @@
 # Page options, layouts, aliases and proxies
 ###
 
+set :layout, 'layouts/application'
+
 # Per-page layout changes:
 #
 # With no layout
@@ -26,7 +28,7 @@ activate :blog do |blog|
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
-  blog.sources = "articles/{year}-{month}-{day}-{title}.html"
+  blog.sources = 'articles/{year}-{month}-{day}-{title}.html'
   # blog.taglink = "tags/{tag}.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
