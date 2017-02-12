620788c5e5ccdd9116f158deda6d7274936cc38d
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-02-12 05:01:19 -0600

    Updated build permalink to use articles subdirectory

--- config.rb
+++ config.rb
@@ -26,9 +26,9 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = 'articles/{year}-{month}-{day}-{title}.html'
   # Matcher for blog source files
-  blog.sources = 'articles/{year}-{month}-{day}-{title}.html'
+  blog.sources = 'articles/{year}-{month}-{day}-{title}'
   # blog.taglink = "tags/{tag}.html"
   blog.layout = 'layouts/application'
   # blog.summary_separator = /(READMORE)/
