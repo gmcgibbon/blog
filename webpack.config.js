63f01b0522dc49b909459d47423ed69f18c9bd10
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-02-20 14:44:05 -0600

    Updated webpack config with awesome-typescript-loader and angular settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
+const Webpack = require('webpack');
+var   helpers = require('./config/helpers');
+
 module.exports = {
   entry: {
     application: [
+      './source/javascripts/polyfill.ts',
+      './source/javascripts/vendor.ts',
       './source/javascripts/main.ts'
     ]
   },
   resolve: {
-    extensions: ['.ts', '.js', '.json'],
+    extensions: ['.ts', '.tsx', '.js', '.json'],
     modules:    ['node_modules', 'bower_components']
   },
   output: {
@@ -17,8 +22,19 @@ module.exports = {
         {
           test:    /source\/javascripts\/.*\.tsx?$/,
           exclude: /node_modules|bower_components|\.webpack-cache/,
-          loader:  'ts-loader'
+          loader:  'awesome-typescript-loader'
         }
     ]
-  }
+  },
+  plugins: [
+    new Webpack.EnvironmentPlugin([
+      'NODE_ENV'
+    ]),
+    new Webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+      helpers.root('./source/javascripts'), // location of your src
+      {} // a map of your routes
+    )
+  ]
 }
