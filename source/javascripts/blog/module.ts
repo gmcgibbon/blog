4fe8852042d4823f7ea04652b7be1acb996a5526
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-03-05 01:33:50 -0600

    Update bootstrap module, remove browser module from blog module

--- source/javascripts/blog/module.ts
+++ source/javascripts/blog/module.ts
@@ -1,15 +1,8 @@
 import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BlogComponent }  from './component';
+import { BlogComponent } from './component';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
   declarations: [
-    BlogComponent
-  ],
-  bootstrap: [
     BlogComponent
   ]
 })
--- source/javascripts/main.ts
+++ source/javascripts/main.ts
@@ -1,9 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode }         from '@angular/core';
-import { BlogModule }              from './blog/module';
+import { ApplicationModule }      from './application/module';
 
 if (process.env.NODE_ENV == 'build') { enableProdMode(); }
 
 document.addEventListener('DOMContentLoaded', () =>
-  platformBrowserDynamic().bootstrapModule(BlogModule)
+  platformBrowserDynamic().bootstrapModule(ApplicationModule)
 );
