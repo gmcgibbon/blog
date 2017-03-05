6f1d3244981431c17af8f02fe52cf0df533f03a3
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-03-04 22:54:29 -0600

    Added blog module bootstrap and element to index

--- source/javascripts/main.ts
+++ source/javascripts/main.ts
@@ -1,8 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode }         from '@angular/core';
+import { BlogModule }              from './blog/module';
 
 if (process.env.NODE_ENV == 'build') { enableProdMode(); }
 
 document.addEventListener('DOMContentLoaded', () =>
-  platformBrowserDynamic().bootstrapModule(null)
+  platformBrowserDynamic().bootstrapModule(BlogModule)
 );
