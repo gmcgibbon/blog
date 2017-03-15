3b25421b080b4a064c63ab9ee47b0dec6fc80371
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-03-15 00:48:27 -0500

    Added article service with #query method

---
+++ source/javascripts/article/service.ts
@@ -0,0 +1,30 @@
+import { Injectable }     from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import { Article }        from '../models/article';
+
+@Injectable()
+export class ArticleService {
+
+  constructor (private http: Http) { }
+
+  private url = `/api/articles.json`;
+
+  query(): Observable<Article> {
+    return this.http
+      .get(this.url)
+      .map(this.map)
+      .catch(this.catch);
+  }
+
+  private map(response: Response) {
+    let json = response.json();
+    return json;
+  }
+
+  private catch(error: any) {
+    let message = `${error.status} - ${error.statusText}`;
+    console.error(message);
+    return Observable.throw(message);
+  }
+}
