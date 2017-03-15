3e707985df50e8f84fb774d8a08ff46bd54f8d64
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-03-15 00:56:38 -0500

    Added article service import and blog service provider to blog module

--- source/javascripts/blog/module.ts
+++ source/javascripts/blog/module.ts
@@ -1,7 +1,17 @@
 import { NgModule }      from '@angular/core';
+import { HttpModule }    from '@angular/http';
+import { ArticleModule } from '../article/module';
 import { BlogComponent } from './component';
+import { BlogService }   from './service';
 
 @NgModule({
+  imports:   [
+    HttpModule,
+    ArticleModule
+  ],
+  providers: [
+    BlogService
+  ],
   declarations: [
     BlogComponent
   ]
