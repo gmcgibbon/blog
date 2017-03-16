b2cce36c274ab7136d8d8c0cd051da50ea783482
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-03-16 00:11:25 -0500

    Added blog resolve injection to blog component

--- source/javascripts/blog/component.ts
+++ source/javascripts/blog/component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute }    from '@angular/router';
+import { Blog }              from '../models/blog';
 
 @Component({
   selector:    'blog',
@@ -6,4 +8,14 @@ import { Component } from '@angular/core';
   styleUrls:  ['./blog/component.css']
 })
 
-export class BlogComponent { }
+export class BlogComponent implements OnInit {
+
+  blog : Blog;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.blog = this.route.snapshot.data.blog;
+  }
+
+}
