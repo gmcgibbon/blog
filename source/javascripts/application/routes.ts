4757b9b43c8c761890939b386f482edc81a8f7c4
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-03-16 00:06:00 -0500

    Added blog resolve to blog component route

--- source/javascripts/application/routes.ts
+++ source/javascripts/application/routes.ts
@@ -1,9 +1,13 @@
-import { Routes }         from '@angular/router';
-import { BlogComponent } from '../blog/component';
+import { Routes }          from '@angular/router';
+import { BlogComponent }   from '../blog/component';
+import { BlogGetResolver } from '../blog/get_resolver';
 
 export const ROUTES: Routes = [
   {
     path: '',
-    component: BlogComponent
+    component: BlogComponent,
+    resolve: {
+      blog: BlogGetResolver
+    }
   }
 ]
