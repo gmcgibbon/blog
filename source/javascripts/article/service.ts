f3f2ad7cce5466dc8d691d6d233dd5d12f24bec4
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-03-18 01:50:37 -0500

    Added article service #get method

--- source/javascripts/article/service.ts
+++ source/javascripts/article/service.ts
@@ -10,15 +10,29 @@ export class ArticleService {
 
   private url = `/api/articles.json`;
 
-  query(): Observable<Article[]> {
+  query() : Observable<Article[]> {
     return this.http
       .get(this.url)
       .map(this.map)
       .catch(this.catch);
   }
 
+  get(id : string) : Observable<Article> {
+    return this.http
+      .get(this.url)
+      .map(response => this.mapSingular(response, id))
+      .catch(this.catch);
+  }
+
   private map(response: Response) {
     let json = response.json();
+    return json;
+  }
+
+  private mapSingular(response: Response, id: string) {
+    let json = this.map(response).find(
+      (article : Article) => article.id == id
+    );
     return json;
   }
 
