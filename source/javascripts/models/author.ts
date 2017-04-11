481ef21f715685c934a015901134d0ed43eb34db
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-04-11 01:33:54 -0500

    Remove social model, keys are dynamic

--- source/javascripts/models/author.ts
+++ source/javascripts/models/author.ts
@@ -1,7 +1,5 @@
-import { Social } from './social';
-
 export class Author {
   name:    string;
   summary: string;
-  social:  Social;
+  social:  object;
 }
--- source/javascripts/models/social.ts
+++
@@ -1,4 +0,0 @@
-export class Social {
-  github:   string;
-  linkedin: string;
-}
